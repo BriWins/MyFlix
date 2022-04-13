6f31ded941528a6311f0782d2615094bb9aec37f
BriWins <95152254+BriWins@users.noreply.github.com>
2022-04-13 17:12:32 -0500

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
-const express = require("express"),   
+const express = require("express"),
+
 morgan = require("morgan");
 
 const app = express();
@@ -9,7 +10,7 @@ app.use(express.static("public"));
 
 const topMovies = [
     {
-        movieTitle: "Black Panther (2018)",
+        movieTitle: "Black Panther",
         director: "Ryan Coogler",
         producer: "Kevin Feige, David J. Grant",
         movieRelease: "February 16, 2018", //Release date for theaters
@@ -42,7 +43,7 @@ const topMovies = [
         movieInfo:"Annie is a single woman whose own life is in a mess. Her best friend, Lillian, is engaged and Annie has to serve as maid of honor. Penniless yet determined to make things perfect, she leads Lillian and other braidsmaids down the wild road to the wedding."
     },
     {
-        movieTitle: "Lady Bird (2017)",
+        movieTitle: "Lady Bird",
         director:"Greta Celeste Gerwig",
         producer: "Scott Rudin, Eli Rush, Evelyn O'Neill",
         movieRelease:"November 22, 2017",
@@ -83,7 +84,7 @@ const topMovies = [
         directorDOB: "April 7, 1939",
         directorPOB: "Detroit, Michigan",   
         directorDOD: "N/A", 
-        movieInfo:"High regarded as one of the greatest films of all time, this movie focuses on the powerful Italian American crime family of Don Vito Corleone. His son relectantly joins the mafia and gets caught in violence and betrayal."
+        movieInfo:"Highly regarded as one of the greatest films of all time, this movie focuses on the powerful Italian American crime family of Don Vito Corleone. His son relectantly joins the mafia and gets caught in violence and betrayal."
     },
     {
         movieTitle: "The Lion King(1994)",
@@ -110,7 +111,7 @@ const topMovies = [
     {
         movieTitle: "The Wizard Of Oz (1939)",
         director:"Victor Flemings",
-        producer: "Victor Fleming, Mervyn LeRoy",
+        producer: "Victor Flemings, Mervyn LeRoy",
         movieRelease:"August 25, 1939",
         genre: "Musical, Fantasy",
         directorDOB: "February 23, 1889",
@@ -119,96 +120,106 @@ const topMovies = [
         directorPOD:"Cottonwood, Arizona", //POD-director's place of death 
         movieInfo:"When a cyclone hits, Dorothy is swallowed up by the cyclone and wakes up in the Land of Oz. She follows the Yellow Brick Road to meet the Wizard of Oz meeting many along the way."
     },
+     
 ]
 
+const users = [{}];
 
-//returns list of all movies
+const bodyParser = require("body-parser"),
+  methodOverride = require("method-override");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// //returns list of all movies
 
 app.get("/movies", (req, res) => {
-    res.json(topMovies);
-  });
-  
-//allows single movie to be searched by title
-
-app.get("/movies/:title", (req, res) => {
-   res.json(movies.find((movies) => {
-       return movies.name === req.params.name
-   }));
+  res.json(topMovies);
 });
 
-//allows user to add movie to favorite list
+// //allows single movie to be searched by title
 
-app.post("/movies/:users/favorites", (req,res) => {
+app.get("/movies/:title", (req, res) => {
+  res.json(
+    movies.find((movies) => {
+      return movies.name === req.params.name;
+    })
+  );
+});
+
+// //allows user to add movie to favorite list
+
+app.post("/movies/:users/favorites", (req, res) => {
   res.status(201);
   res.send("The movie has been added to favorites list.");
 });
 
-//allows user to remove movie from favorite list
+// //allows user to remove movie from favorite list
 
-app.delete("/movies/:users/favorites", (req,res) => {
+app.delete("/movies/:users/favorites", (req, res) => {
   res.send("The movie has been removed from favorites list.");
 });
 
-//allows new user to register an account
+// //allows new user to register an account
 
-app.post("/users/register", (req,res) => {
-    let newUsers = req.body;
-    
-    if (!newUsers.name) {
-        const message = "Missing name for account.";
-        res.status(400).send(message);
-    } else {
-        newUsers.id = uuid.v4();
-        newUsers.push(newUsers);
-        res.status(201).send(newUsers);
-    }
+app.post("/users/register", (req, res) => {
+  let newUser = req.body;
+  if (!newUser.name) {
+    const message = "Missing name for account.";
+    res.status(400).send(message);
+  } else {
+    // newUsers.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
 });
 
-//allows user to delete their account
+// //allows user to delete their account
 
 app.delete("/users/:users", (req, res) => {
   res.send("Your account has been removed");
 });
 
-//allows user to update their account
+// //allows user to update their account
 
 app.put("/users/:users", (req, res) => {
   res.send("Your account has been updated.");
 });
 
-// //Return data about a director (bio, birth year, death year) by name
+// // //Return data about a director (bio, birth year, death year) by name
+
 app.get("/movies/directors/:names", (req, res) => {
-  res.send("Details about the director: " + req.params.name);
+  res.send("Details about the director: " + req.params.names);
 });
 
 //returns movie by filtering data by genre
 
 app.get("/movies/:genres", (req, res) => {
-  res.json(movies.find((genres) =>
-    { return movies.genres === req.params.genres }));
+  res.json(
+    movies.find((genres) => {
+      return movies.genres === req.params.genres;
+    })
+  );
 });
-    
-app.get("/documentation", (req, res) => {                  
-    res.sendFile("Public/documentation.html", { root: __dirname });
-  });
 
-//function to log all errors
+// returns web developer API instructions
+app.get("/documentation", (req, res) => {
+  res.sendFile("Public/documentation.html", { root: __dirname });
+});
 
-const bodyParser = require('body-parser'),
-methodOverride = require('method-override');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(bodyParser.json());
-app.use(methodOverride());
+// //function to log all errors
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("UH-OH! Something broke!");
+  console.error(err.stack);
+  res.status(500).send("UH-OH! Something broke!");
 });
 
 app.listen(5500, () => {
-     console.log('Your app is listening on port 5500.');
+  console.log("Your app is listening on port 5500.");
 });
