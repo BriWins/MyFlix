500b768e8411afb709500493855478e6921804a6
BriWins <95152254+BriWins@users.noreply.github.com>
2022-04-30 13:10:52 -0500

    Deleted topMovies data
    Data now included within MongoDB

--- JS/index.js
+++ JS/index.js
@@ -8,132 +8,6 @@ app.use(morgan("common"));
 
 app.use(express.static("public"));
 
-const topMovies = [
-     {
-        movieTitle: "Black Panther",
-        director: "Ryan Coogler",
-        bio:"Ryan Kyle Coogler is an African-American filmmaker and producer who is from Oakland, California.",
-        movieRelease: "February 16, 2018", //Release date for theaters
-        genre: "Adventure",
-        directorDOB: "May 23, 1986", //DOB- director's date of birth
-        directorPOB: "Oakland, CA",   //POB-director's place of birth
-        directorDOD: "N/A", //DOD-director's date of death
-        movieInfo:"American superhero film based on the Marvel Comics character, Blank Panther, depicted as the King and protector of the fictional African nation of Wakanda."
-    },
-    {
-        movieTitle: "Mad Max: Fury Road",
-        director:"George Miller",
-        bio: "George Miller is an Australian film director, screenwriter, producer, and former medical doctor.",
-        movieRelease: "March 3, 1945",
-        genre: "Adventure",
-        directorDOB: "May 23, 1986",
-        directorPOB: "Chinchilla, Queensland, Australia",   
-        directorDOD: "N/A", 
-        movieInfo:"In a post-apocalyptic desert wasteland, Fury Road follows Max Rockatansky to flee from cult leader Immortan Joe and his army."
-    },
-    {
-        movieTitle: "Braidsmaids",
-        director:"Paul Feig",
-        bio:"Paul Samuel Feig is an American actor, comedian and filmmaker.",
-        movieRelease:"May 13, 2011",
-        genre: "Comedy",
-        directorDOB: "September 17, 1962",
-        directorPOB: "Mount Clemens, Michigan",   
-        directorDOD: "N/A", 
-        movieInfo:"Annie is a single woman whose own life is in a mess. Her best friend, Lillian, is engaged and Annie has to serve as maid of honor. Penniless yet determined to make things perfect, she leads Lillian and other braidsmaids down the wild road to the wedding."
-    },
-    {
-        movieTitle: "Lady Bird",
-        director:"Greta Celeste Gerwig",
-        bio:"Greta Celeste Gerwig is an American actress, writer, and director.",
-        movieRelease:"November 22, 2017",
-        genre: "Comedy",
-        directorDOB: "August 4, 1983",
-        directorPOB: "Sacramento, California",   
-        directorDOD: "N/A", 
-        movieInfo:"A teenager navigates a loving but turbulent relationship with her mother during her eventful senior year of high school."
-    },
-    {
-        movieTitle: "Inception",
-        director:"Christopher Nolan",
-        bio:"Christopher Nolan is a British-American film director, producer, and screenwriter.",
-        movieRelease:"July 16, 2010",
-        genre: "Sci-Fi", // Sci-Fi is science fiction
-        directorDOB: "July 30, 1970",
-        directorPOB: "London, England",   
-        directorDOD: "N/A", 
-        movieInfo:"Dom Cobb is a thief with the rare ability to enter people's dreams and steal their secrets from their subconscious. He is charged with the task of planting an idea in someone's mind. Will he succeed?"
-    },
-    {
-        movieTitle: "Spider-Man: Into the Spider-Verse", //spiderman.png for database
-        director:"Peter Ramsey",
-        bio:"Peter A. Ramsey is an American illustrator, storyboard artist, story artist, film director, and film producer.",
-        movieRelease:"December 14, 2018",
-        genre: "Adventure",
-        directorDOB: "December 23, 1962",
-        directorPOB: "Crenshaw, California",   
-        directorDOD: "N/A", 
-        movieInfo:"A 2018 American computer-animated super hero film featuring the Marvel Comics character, Spider-Man."
-    },
-    {
-        movieTitle: "The Godfather",
-        director:"Francis Ford Coppola",
-        bio:"Francis Ford Coppola is an American film director, producer, and screenwriter.",
-        movieRelease:"March 15, 1972",
-        genre: "Drama",
-        directorDOB: "April 7, 1939",
-        directorPOB: "Detroit, Michigan",   
-        directorDOD: "N/A", 
-        movieInfo:"Highly regarded as one of the greatest films of all time, this movie focuses on the powerful Italian American crime family of Don Vito Corleone. His son relectantly joins the mafia and gets caught in violence and betrayal."
-    },
-    {
-        movieTitle: "The Lion King",
-        director:"Rob Minkoff",
-        bio:"Robert Ralph (Rob) Minkoff is an American director, screenwriter, producer and animator.",
-        movieRelease:"June 24, 1994",
-        genre: "Animation",
-        directorDOB: "August 11, 1962",
-        directorPOB: "Palo Alto, California",   
-        directorDOD: "N/A", 
-        movieInfo:"A Disney animated feature follows the adventures of the young lion, Simba, the heir of his father, Mufasa. Simba's father is killed by a stampede, but Simba's uncle Scar was behind his father's death. Now an adult, Simba returns home to take back his homeland."
-    },
-    {
-        movieTitle: "The Shining",
-        director:"Stanley Kubrick",
-        bio:"Stanley Kubrick was an American film director, producer, screenwriter, and photographer.",
-        movieRelease:"May 23, 1980",
-        genre: "Thriller",
-        directorDOB: "July, 26, 1928",
-        directorPOB: "Bronx, New York",   
-        directorDOD: "March 7, 1999", 
-        movieInfo:"A murderer is hell bent on terrorizing Jack Torrance's family as a hotel's dark secrets unravel."
-    },
-    {
-        movieTitle: "The Wizard Of Oz",
-        director:"Victor Fleming",
-        bio:"Victor Lonzo Fleming was an American film director, cinematographer, and producer.",
-        movieRelease:"August 25, 1939",
-        genre: "Musical",
-        directorDOB: "February 23, 1889",
-        directorPOB: "La Canada, California",   
-        directorDOD: "January 6, 1949",
-        movieInfo:"When a cyclone hits, Dorothy is swallowed up by the cyclone and wakes up in the Land of Oz. She follows the Yellow Brick Road to meet the Wizard of Oz meeting many along the way."
-    },
-    {
-      movieTitle: "Gone With The Wind",
-      director:"Victor Fleming",
-      bio:"Victor Lonzo Fleming was an American film director, cinematographer, and producer.",
-      movieRelease:"August 25, 1939",
-      genre: "Drama",
-      directorDOB: "February 23, 1889",
-      directorPOB: "La Canada, California",   
-      directorDOD: "January 6, 1949",
-      movieInfo:"The daughter of a Georgia plantation owner conducts a turbulent romance with a roguish profiteer during the American Civil War."
-  },
-]
-
-const users = [{}];
-
 const bodyParser = require("body-parser"),
   methodOverride = require("method-override");
 
@@ -146,7 +20,8 @@ app.use(
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-// //returns list of all movies
+
+//returns list of all movies
 
 app.get("/movies", (req, res) => {
   res.json(topMovies);
