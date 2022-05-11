aaa9279356429a1b10ddf14c2f766de0ee877ca6
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-11 10:08:15 -0500

    Create models.js

---
+++ JS/models.js
@@ -0,0 +1,46 @@
+---------------------------Importing Mongoose-------------------------------- 
+
+const mongoose = require("mongoose");
+
+---------------------------Movie Schema-------------------------------- 
+
+let movieSchema = mongoose.Schema({
+    Title: { type: String, required: true},
+    Description: {type: String, required: true},
+    Featured:Boolean,
+    ImgPath: String,
+    Rating: String,
+    ReleaseDate: Date,
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Biography: String,
+        Birthdate: Date,
+        Birthplace: String,
+        Deathplace: String
+    },
+    Actors: [String]
+});
+
+---------------------------User Schema-------------------------------- 
+
+let userSchema = mongoose.Schema({
+    Username: String,
+    Password: String,
+    Email: String,
+    Birthdate: Date,
+    Favorites: [String]
+});
+
+---------------------------Exportation-------------------------------- 
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+
+
