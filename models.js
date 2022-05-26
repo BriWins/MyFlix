13ffe370219a3c83e69b99249f839380967734a7
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-26 08:30:30 -0500

    Update models.js

--- models.js
+++ models.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-//---------------------------Movie Schema-------------------------------- 
+/* ---------------------------Movie Schema-------------------------------- */
 
 let movieSchema = mongoose.Schema({
     Title: { type: String, required: true},
@@ -21,37 +21,37 @@ let movieSchema = mongoose.Schema({
         Biography: String,
         Birthdate: Date,
         Birthplace: String,
-        Deathdate: String
+        Deathplace: String
     },
     Actors: [String]
 });
 
-//---------------------------User Schema-------------------------------- 
+/* ---------------------------User Schema-------------------------------- */
 
 let userSchema = mongoose.Schema({
     Username: String,
     Password: String,
     Email: String,
     Birthdate: Date,
-    Favorites: [String]
+    Favorites: [{type: String, ref: "Movie"}]
 });
 
-//---------------------------Hashing Function-------------------------------- 
+//---------------------------Hashing Passwords-------------------------------- 
 
 userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password,10);
+    return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function(password) {
     return bcrypt.compareSync(password, this.Password);
 };
 
-//---------------------------Exportation-------------------------------- 
+/* ---------------------------Exportation-------------------------------- */
 
 let Movie = mongoose.model("Movie", movieSchema);
 let User = mongoose.model("User", userSchema);
 
-module.exports.Movie = Movie;
-module.exports.User = User;
+module.exports.movies = Movie;
+module.exports.users = User;
 
 
